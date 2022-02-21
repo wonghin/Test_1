7d87d7ff531fdddf868e910bde114e8d1d702d6b
Andy <chwongbq@connect.ust.hk>
2022-02-21 17:38:56 +0800

    Initial commit

---
+++ second-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react'
+import Words from './components/Words';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Words />
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ second-app/src/components/Words.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Words extends Component {
+  render() {
+    return (
+        <div>
+          Hello everyone, my name is Andy
+        </div>
+    )
+  }
+}
+
+export default Words
